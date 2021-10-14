35ef7280e7acf423ab00232d79368fd373b92f33
ganfanle <92416877+laoganmaqin@users.noreply.github.com>
2021-10-14 11:34:28 +0800

    abb

---
+++ section2/w7_Project/apiserver/router/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router();
+module.exports = router;
+
+const userRouter = require('./user')
+const goodsRouter = require('./goods')
+const regRouter = require('./reg')
+const loginRouter = require('./login')
+
+// 允许跨域
+router.use(function(req,res,next){
+    // res.header('Access-Control-Allow-Origin','*');
+    res.set({
+        'Access-Control-Allow-Origin':'http://localhost:8080'
+    })
+
+    next();
+})
+
+// /api/user
+router.use('/user',userRouter)
+router.use('/goods',goodsRouter)
+router.use('/reg',regRouter)
+router.use('/login',loginRouter)
